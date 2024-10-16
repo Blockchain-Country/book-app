d288f8e6705496d25dcd492aba0b6169263536ee
Kot Anton <707myemail@gmail.com>
2024-10-15 21:19:41 -0400

    implemented language via i18n lib and added error reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,27 @@
+import { useTranslation } from 'react-i18next'
 import BookForm from './components/bookForm/BookForm.js'
 import Filter from './components/filter/Filter.js'
 import BookList from './components/bookList/BookList.js'
+import './utils/i18n.js'
 import './App.css'
 
 function App() {
+  const { t, i18n } = useTranslation()
+
+  const handleLanguageChange = (event) => {
+    i18n.changeLanguage(event.target.value)
+  }
+
   return (
     <div className="app">
       <header className="app-header" data-testid="app_main_header">
-        <h1>Book Library App</h1>
+        <h1>{t('appName')}</h1>
+        <div className="language-selector">
+          <select onChange={handleLanguageChange}>
+            <option value="en">English</option>
+            <option value="es">Español</option>
+          </select>
+        </div>
       </header>
       <main className="app-main">
         <div className="app-left-column">
---
+++ frontend/src/redux/slices/ErrorSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = ''
+
+const errorSlice = createSlice({
+  name: 'ERROR',
+  initialState,
+  reducers: {
+    SET_ERROR: (state, action) => {
+      return action.payload
+    },
+    CLEAR_ERROR: () => {
+      return initialState
+    },
+  },
+})
+
+export const { SET_ERROR, CLEAR_ERROR } = errorSlice.actions
+
+export const selectError = (state) => state.ERROR
+
+export default errorSlice.reducer
